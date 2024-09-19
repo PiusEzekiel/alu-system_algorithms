84e8c9ac4dc48751fc97b9d6dc482e4aa0f7b693
PiusEzekiel <e.pius1@alustudent.com>
2024-09-19 00:17:21 -0700

    0-graph_create.c

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -12,11 +12,11 @@ graph_t *graph_create(void)
 
 	new_graph = malloc(sizeof(graph_t));
 	if (!new_graph)
-		return NULL;
+		return (NULL);
 
 	new_graph->nb_vertices = 0;
 
-	new_graph->vertices = NULL;
+	new_graph->vertices = (NULL);
 
 	return new_graph;
 }
